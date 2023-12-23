b2a5063f24d572243b51792adb976174b860f621
Daniel Rangel <000.drf@gmail.com>
2023-12-23 14:43:56 -0800

    JWT verification middleware

---
+++ src/middleware/verifyJwt.ts
@@ -0,0 +1,42 @@
+import { Request, Response } from 'express';
+import asyncHandler from 'express-async-handler';
+import jwt, { JsonWebTokenError } from 'jsonwebtoken';
+
+export const verifyJwt = asyncHandler(
+    async (
+        req: Request & { user?: { email: string; uid: string } },
+        res: Response,
+        next,
+    ) => {
+        const authHeader = req.headers.authorization;
+
+        if (!authHeader?.startsWith('Bearer ')) {
+            res.status(401).json({ message: 'Missing auth header.' });
+            return;
+        }
+
+        const secreteAt = process.env.ACCESS_TOKEN_SECRETE;
+        const secreteRt = process.env.REFRESH_TOKEN_SECRETE;
+
+        if (!secreteAt || !secreteRt) {
+            res.status(500).json({ message: 'Internal server error.' });
+            return;
+        }
+
+        const token = authHeader.split(' ')[1];
+
+        jwt.verify(
+            token,
+            secreteAt,
+            (error: JsonWebTokenError | null, decoded: any) => {
+                if (error || !decoded.user) {
+                    res.status(403).json({ message: 'Invalid token' });
+                    return;
+                }
+
+                req.user = decoded.user;
+                next();
+            },
+        );
+    },
+);
